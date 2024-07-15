8a0472d2e74005251741c64efec2a990c7985de5
Pasha Rodideal <pasharodideal@gmail.com>
2024-07-15 09:17:03 -0400

    Getting book reviews based on isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -65,8 +65,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+        res.status(200).send(books[isbn].reviews);
+    }
+    else {
+        res.status(404).send("Unable to find book with that isbn.")
+    }
 });
 
 module.exports.general = public_users;
